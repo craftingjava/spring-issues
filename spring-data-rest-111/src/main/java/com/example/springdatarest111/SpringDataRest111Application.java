2483f5bee376fd637dc656395f8b54d547de30c5
Laszlo Csontos <github@laszlocsontos.com>
2018-04-20 08:44:07 +0200

    List handler mappings with their orders

--- spring-data-rest-111/src/main/java/com/example/springdatarest111/SpringDataRest111Application.java
+++ spring-data-rest-111/src/main/java/com/example/springdatarest111/SpringDataRest111Application.java
@@ -1,5 +1,7 @@
 package com.example.springdatarest111;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.LongStream;
 import javax.persistence.Entity;
@@ -8,18 +10,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.util.comparator.Comparators;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 
+@Slf4j
 @SpringBootApplication
 public class SpringDataRest111Application {
+
+    @Autowired
+    ListableBeanFactory listableBeanFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataRest111Application.class, args);
@@ -30,6 +41,14 @@ public class SpringDataRest111Application {
         return args -> {
             LongStream.range(0, 10).mapToObj(Record::of).iterator()
                     .forEachRemaining(recordRepository::save);
+
+            listableBeanFactory.getBeansOfType(HandlerMapping.class)
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() instanceof Ordered)
+                    .map(it -> new SimpleEntry<>(it.getKey(), ((Ordered)it.getValue()).getOrder()))
+                    .sorted(Comparator.comparing(SimpleEntry::getValue))
+                    .forEach(it -> log.info("bean, order -> {}, {}", it.getKey(), it.getValue()));
         };
     }
 }
